851f70af68453d2e6b47e9c8aa2f28fa4ead4000
Harvey <ch4ch4@gmail.com>
2014-02-20 13:20:24 -0800

    added basic cloudcode template

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,6 +1,20 @@
+// App variables
+var ALERT_RADIUS = 20;
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.beforeSave("Ping", function (request, response) {
+    var user = request.object.get("User");
+    var location = request.object.get("Location");
+
+    //insert code here to check ping location and trigger alerts
+
+    response.success("PONG");
 });
+
+//TODO: send sms through twilio
+
+//TODO: panic button 
+
+function getPhonenumberForUser(user) {
+    Parse.Cloud.useMasterKey();
+    var number = user.get("phonenumber");
+}
