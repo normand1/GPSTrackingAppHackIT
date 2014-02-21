de1b3913729a1ee251367e83b61a37d50dbf0e61
Harvey <ch4ch4@gmail.com>
2014-02-21 05:13:09 -0800

    modified monitor page to use new cloud code function

--- parse/public/js/hackit.js
+++ parse/public/js/hackit.js
@@ -1,27 +1,41 @@
 
-var User = Parse.Object.extend("User");
-var query = new Parse.Query(User);
-query.get("BBBRCdOfJQ", {
-  success: function(user) {
-    // The object was retrieved successfully.
-    console.log("retreived successfully");
-    var test = (user.get('outOfBounds'));
-    console.log(test);
-    if (test === true)
-    {
-    changeStatusToWarn();
-    }
-    else
-    {
-    changeStatusToSafe();
-    }
-  },
-  error: function(object, error) {
-    // The object was not retrieved successfully.
-    // error is a Parse.Error with an error code and description.
+//var User = Parse.Object.extend("User");
+//var query = new Parse.Query(User);
+//query.get("BBBRCdOfJQ", {
+//  success: function(user) {
+//    // The object was retrieved successfully.
+//    console.log("retreived successfully");
+//    var test = (user.get('outOfBounds'));
+//    console.log(test);
+//    if (test === true)
+//    {
+//    changeStatusToWarn();
+//    }
+//    else
+//    {
+//    changeStatusToSafe();
+//    }
+//  },
+//  error: function(object, error) {
+//    // The object was not retrieved successfully.
+//    // error is a Parse.Error with an error code and description.
     
-    console.log(Parse.Error);
-  }
+//    console.log(Parse.Error);
+//  }
+//});
+
+Parse.Cloud.run('CheckIfUserOutOfBounds', { "userId": "BBBRCdOfJQ" }, {
+    success: function (result) {
+        //console.log("success");
+        if (result === true) {
+            changeStatusToWarn();
+        } else {
+            changeStatusToSafe();
+        }
+    },
+    error: function (error) {
+        //console.log("fail");
+    }
 });
 
 var changeStatusToWarn = function() {
